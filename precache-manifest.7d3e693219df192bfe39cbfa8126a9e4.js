f063f697d09641f5a248fa6f200ef4d1c0141aea
Axedez <45712870+Axedez@users.noreply.github.com>
2020-08-17 18:22:59 +0300

    Updates

---
+++ precache-manifest.7d3e693219df192bfe39cbfa8126a9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b01db17b97e9087fa5fe3680815ce1",
+    "url": "/calc-money/index.html"
+  },
+  {
+    "revision": "1d751ab9753026aaed36",
+    "url": "/calc-money/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/calc-money/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc-money/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d751ab9753026aaed36",
+    "url": "/calc-money/static/js/main.2fe2c8e3.chunk.js"
+  },
+  {
+    "revision": "7f94107833afc5735ccc",
+    "url": "/calc-money/static/js/runtime-main.b19a42aa.js"
+  }
+]);
